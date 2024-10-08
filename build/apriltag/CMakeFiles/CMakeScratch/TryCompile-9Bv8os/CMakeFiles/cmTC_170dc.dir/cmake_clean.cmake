c06e2503fd0ce10609a6229054354bc0595d3d3e
Zhang Yf <759094438@qq.com>
2024-10-08 10:34:25 +0800

    change file type

---
+++ build/apriltag/CMakeFiles/CMakeScratch/TryCompile-9Bv8os/CMakeFiles/cmTC_170dc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_170dc.dir/CMakeCCompilerABI.c.o"
+  "cmTC_170dc"
+  "cmTC_170dc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_170dc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
