9d2e8947d77432b80f73ae758dca594aada08307
DUDULRX <2849678712@qq.com>
2026-09-03 16:07:58 +0800

    modified:   build_first.sh

--- build_first.sh
+++ build_first.sh
@@ -227,6 +227,13 @@ sudo apt-get install -y \
   speech-dispatcher \
   speech-dispatcher-espeak \
   espeak-ng \
+  gstreamer1.0-tools \
+  gstreamer1.0-plugins-base \
+  gstreamer1.0-plugins-good \
+  gstreamer1.0-plugins-bad \
+  gstreamer1.0-plugins-ugly \
+  gstreamer1.0-libav \
+  gstreamer1.0-nice \
   gstreamer1.0-rtsp \
   software-properties-common \
   curl \
